0d1392d60b7cf7fc48fd75fbced800774656a117
Dhstefanoski <dhstefanoski@minha.fag.edu.br>
2026-03-26 19:37:29 -0300

    Entrega Atividade 5

---
+++ primeirobi/atv5/calculadora_gorjeta.js
@@ -0,0 +1,6 @@
+const valorConta = 200;
+const percentaulgorjeta = 10;
+const gorjeta = valorConta * (percentaulgorjeta / 100)
+const valorTotal = valorConta + gorjeta
+
+console.log("Valor da Conta: " + valorConta + ", Gorjeta (15%): " + gorjeta + ", Total a Pagar: " + valorTotal);
